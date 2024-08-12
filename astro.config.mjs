35a9465d13bfc7044191c019e8bf49b534a4ca61
Jean Nissen <jeanwebster@users.noreply.github.com>
2024-08-12 13:59:15 -0700

    Update astro.config.mjs
    Removed
    // https://astro.build/config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
     build: {
         format: 'file'
-    site: 'https://jeanwebster.github.io/',
-    base: '/wwubudget',
+        site: 'https://jeanwebster.github.io/',
+        base: '/wwubudget',
       },
 });
