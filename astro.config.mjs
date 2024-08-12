ffc96a1353dd17f6c0139b8f8632f1be7b2efd24
Jean Nissen <jeanwebster@users.noreply.github.com>
2024-08-12 14:06:49 -0700

    Update astro.config.mjs
    changed single quotes ' to "

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
     build: {
-        format: 'file'
-        site: 'https://<random-string>.pages.github.io/',
-        base: '/wwubudget',
+        format: "file"
+        site: "https://<random-string>.pages.github.io/",
+        base: "/wwubudget",
       },
 });
